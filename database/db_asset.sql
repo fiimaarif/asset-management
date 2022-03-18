385f780bf49d2b03ad33d30aefc02da3a3179537
fiimaarif <fiimaarif44@gmail.com>
2022-03-18 17:45:27 +0700

    update database

--- database/db_asset.sql
+++ database/db_asset.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Mar 2022 pada 18.22
+-- Waktu pembuatan: 18 Mar 2022 pada 11.44
 -- Versi server: 10.3.16-MariaDB
 -- Versi PHP: 7.3.7
 
